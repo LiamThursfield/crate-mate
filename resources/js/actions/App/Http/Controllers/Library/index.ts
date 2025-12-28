2a859664a41be64879efbfdab8b1ad715bcd4f05
Liam <liam3006x@gmail.com>
2025-12-28 22:21:58 +0000

    Cleanup gitignore (#15)

---
+++ resources/js/actions/App/Http/Controllers/Library/index.ts
@@ -0,0 +1,7 @@
+import LibraryArtistController from './LibraryArtistController'
+
+const Library = {
+    LibraryArtistController: Object.assign(LibraryArtistController, LibraryArtistController),
+}
+
+export default Library
