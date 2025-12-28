ae697bcb8a412ac5729caf2765e8f9cfeef52114
Liam <liam3006x@gmail.com>
2025-12-28 23:22:32 +0000

    pnpm build

--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -1,9 +1,9 @@
-import Library from './Library'
-import Settings from './Settings'
+import Library from './Library';
+import Settings from './Settings';
 
 const Controllers = {
     Library: Object.assign(Library, Library),
     Settings: Object.assign(Settings, Settings),
-}
+};
 
-export default Controllers
+export default Controllers;
